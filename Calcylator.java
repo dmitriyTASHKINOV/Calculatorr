874b1785a7f45597c6749949acef92fa4c606a89
dmitriyTASHKINOV <167447238+dmitriyTASHKINOV@users.noreply.github.com>
2024-04-24 22:07:14 +0300

    Update Calcylator.java

--- Calcylator.java
+++ Calcylator.java
@@ -1,64 +1,70 @@
 import java.util.Scanner;
 
 public class Calcylator {
-    public static void main(String[] args) throws Exception {
+    public static String calc(String input) throws Exception {
+
+        String[] sing = new String[]{"+", "-", "*", "/"};
+        String[] split = new String[]{"\\+", "-", "\\*", "/"};
         Convert convert = new Convert();
-        Scanner scanner = new Scanner(System.in);
-      String[] sing = new String[]{"+","-","*","/"};
-      String[] split = new String[]{"\\+","-","\\*","/"};
-        System.out.println("Введите число");
-        String exp = scanner.nextLine();
         int singsave = -1;
-            for (int i = 0; i < sing.length; i++) {
-                if (exp.contains(sing[i])) {
-                   singsave = i;
-                }
+        for (int i = 0; i < sing.length; i++) {
+            if (input.contains(sing[i])) {
+                singsave = i;
             }
-            if(singsave==-1){
+        }
+        if (singsave == -1) {
+            throw new Exception("Некорректное выражение");
+        }
+        String[] number = input.split(split[singsave]);
+        if (convert.Roman(number[0]) == convert.Roman(number[1])) {
+            if (number.length > 2) {
+                throw new Exception("Вы ввели больше двух чисел");
+            }
+            int a, b;
+            boolean isRoman = convert.Roman(number[0]);
+            if (isRoman) {
+                a = convert.romanToInt(number[0]);
+                b = convert.romanToInt(number[1]);
+            } else {
+                a = Integer.valueOf(number[0]);
+                b = Integer.valueOf(number[1]);
+            }
+            if (a > 10 || b > 10) {
                 throw new Exception("Некорректное выражение");
             }
-            String[] number = exp.split(split[singsave]);
-            if (convert.Roman(number[0])==convert.Roman(number[1])){
-                 if(number.length>2){
-                    throw new Exception("Вы ввели больше двух чисел");
-                }
-             int a,b;
-             boolean isRoman=convert.Roman(number[0]);
-             if(isRoman){
-                 a = convert.romanToInt(number[0]);
-                 b= convert.romanToInt(number[1]);
-             }else {
-                 a=Integer.valueOf(number[0]);
-                 b=Integer.valueOf(number[1]);
-             }if(a>10||b>10){
-                    throw new Exception("Некорректное выражение");
-                }
-                int rezylt=0;
-             switch (sing[singsave]){
-                 case "+":
-              rezylt = a+b;
-              break;
-                 case "-":
-               rezylt = a-b;
-               break;
-                 case "*":
-                     rezylt = a*b;
-                     break;
-                 case "/":
-                     rezylt = a/b;
-                     break;
-             }
-                if (isRoman) {
-                    System.out.println(convert.intToRoman(rezylt));
-                }else {
-                    System.out.println(rezylt);
-                }
-            }else {
-                throw new Exception("Некорректное выражение");
+            int rezylt = 0;
+            switch (sing[singsave]) {
+                case "+":
+                    rezylt = a + b;
+                    break;
+                case "-":
+                    rezylt = a - b;
+                    break;
+                case "*":
+                    rezylt = a * b;
+                    break;
+                case "/":
+                    rezylt = a / b;
+                    break;
             }
+            if (isRoman) {
+                return convert.intToRoman(rezylt);
+            } else {
+                return Integer.toString(rezylt);
+            }
+        } else {
+            throw new Exception("Некорректное выражение");
+        }
     }
 
+    public static void main(String[] args) throws Exception {
+        while (true){
+        System.out.println("Введите число");
+        Scanner scanner = new Scanner(System.in);
+        String exp = scanner.nextLine();
+        System.out.println(calc(exp));}
     }
+}
 
 
 
